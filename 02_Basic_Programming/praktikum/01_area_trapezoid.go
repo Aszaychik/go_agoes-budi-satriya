86d19c4070338f497bff8805686713e1337c0215
AsZaychik <aszaychik@gmail.com>
2023-08-13 23:35:14 +0700

    soal 01 area trapezoid

---
+++ 02_Basic_Programming/praktikum/01_area_trapezoid.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func AreaOfTrapezoid(base1 float32, base2 float32, height float32) float32 {
+	return 0.5 * (base1 + base2) * height
+}
+
+func main() {
+	fmt.Println("Masukkan base1: ")
+	var base1 float32
+	fmt.Scan(&base1)
+
+	fmt.Println("Masukkan base2: ")
+	var base2 float32
+	fmt.Scan(&base2)
+
+	fmt.Println("Masukkan height: ")
+	var height float32
+	fmt.Scan(&height)
+
+	result := AreaOfTrapezoid(base1, base2, height)
+
+	fmt.Println(result)
+}
