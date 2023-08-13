067437d4180236e43beb66160f8416c250ff67c6
AsZaychik <aszaychik@gmail.com>
2023-08-13 23:35:40 +0700

    soal 03 grade

---
+++ 02_Basic_Programming/praktikum/03_grade.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func GradeCalculation(nilai int) string {
+	switch {
+	case nilai >= 80 && nilai <= 100:
+		return "A"
+	case nilai >= 65 && nilai < 80:
+		return "B"
+	case nilai >= 50 && nilai < 65:
+		return "C"
+	case nilai >= 35 && nilai < 50:
+		return "D"
+	case nilai >= 0 && nilai < 35:
+		return "E"
+	default:
+		return "Nilai Invalid"
+	}
+}
+
+func main() {
+	var nilai int
+	fmt.Println("Masukkan nilai: ")
+	fmt.Scan(&nilai)
+
+	result := GradeCalculation(nilai)
+
+	fmt.Println(result)
+}
