c5a0e06c485aeeac7f5b8dd14a4542b4ccbbcb9a
AsZaychik <aszaychik@gmail.com>
2023-08-13 23:35:52 +0700

    soal 04 fizz buzz

---
+++ 02_Basic_Programming/praktikum/04_fizz_buzz.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func FizzBuzz(n int) []string {
+	results := make([]string, n)
+	for i := 1; i <= n; i++ {
+			if i%3 == 0 && i%5 == 0 {
+					results[i-1] = "FizzBuzz"
+			} else if i%3 == 0 {
+					results[i-1] = "Fizz"
+			} else if i%5 == 0 {
+					results[i-1] = "Buzz"
+			} else {
+					results[i-1] = fmt.Sprintf("%d", i)
+			}
+	}
+	return results
+}
+
+
+func main() {
+	result := FizzBuzz(100)
+
+	fmt.Println(result)
+}
