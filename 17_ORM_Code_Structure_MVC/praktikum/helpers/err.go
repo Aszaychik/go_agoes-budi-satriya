0e25970ed495e8c3d62edc00b7514aec3a8d4a1a
AsZaychik <aszaychik@gmail.com>
2023-09-24 03:22:59 +0700

    17 "i hate panic :( "

--- 17_ORM_Code_Structure_MVC/praktikum/helpers/err.go
+++ 17_ORM_Code_Structure_MVC/praktikum/helpers/err.go
@@ -5,7 +5,6 @@ import "github.com/sirupsen/logrus"
 func LogIfError(message string, err error) error {
   if err != nil {
     logrus.Error(message, err.Error())
-    panic(err)
   }
   return nil
 }
