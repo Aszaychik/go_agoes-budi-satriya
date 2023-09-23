69aeeb9dfb4b808b950f528cad094b647a497166
AsZaychik <aszaychik@gmail.com>
2023-09-24 01:18:15 +0700

    17 setup helper

---
+++ 17_ORM_Code_Structure_MVC/praktikum/helpers/err.go
@@ -0,0 +1,11 @@
+package helpers
+
+import "github.com/sirupsen/logrus"
+
+func LogIfError(message string, err error) error {
+  if err != nil {
+    logrus.Error(message, err.Error())
+    panic(err)
+  }
+  return nil
+}
