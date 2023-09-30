cc4a1d0c6a097573fc15721481952de10614a5e8
AsZaychik <aszaychik@gmail.com>
2023-09-30 09:14:11 +0700

    19 Rest API convert user response to user domain

---
+++ 19_Unit_Testing/praktikum/rest-api/helper/convert_response.go
@@ -0,0 +1,12 @@
+package helper
+
+import (
+	"unit_testing/model/domain"
+	"unit_testing/model/web"
+)
+
+func UserDomainToUserLoginResponse(user *domain.User) web.UserLoginResponse {
+	return web.UserLoginResponse{
+		Email: user.Email,
+	}
+}
