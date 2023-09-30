b51448e09e4724144f58f6237454baebf1770316
AsZaychik <aszaychik@gmail.com>
2023-09-30 21:28:44 +0700

    19 rest api user request model

--- 19_Unit_Testing/praktikum/rest-api/model/web/user_request.go
+++ 19_Unit_Testing/praktikum/rest-api/model/web/user_request.go
@@ -10,3 +10,9 @@ type UserLoginRequest struct {
 	Email    string `json:"email" validate:"required,email,min=1,max=255"`
 	Password string `json:"password" validate:"required,max=255"`
 }
+
+type UserUpdateRequest struct {
+	Name     string `json:"name" validate:"min=1,max=255"`
+	Email    string `json:"email" validate:"email,min=1,max=255"`
+	Password string `json:"password" validate:"min=8,max=255"`
+}
